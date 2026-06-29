1b8db0ac7d09613001ea2feb36724e1175567c75
zm26409 <zidan_1211@icloud.com>
2026-06-29 11:30:47 +0900

    Update myls.js

--- myls.js
+++ myls.js
@@ -1,11 +1,8 @@
 alert('Hello,syujiro!);
 alert(1+1);
 alert(Math.sqrt(1+1));
-・２の平方根と３の平方根の和を計算しなさい。
-・１を０で割るとどうなるか、試してみなさい。
-・０を０で割るとどうなるか、試してみなさい。
-・2+3
-・'2'+'3'
-・Number('2')+Number('3')
-・'2'+3
-・Number('2')+3
+alert(2+3)
+alert('2'+'3')
+alert(Number('2')+Number('3')
+alert'2'+3
+alertNumber('2');3
